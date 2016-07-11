7f8f4340e2b1f908d1942537f4ae84b8954d42f9
DanzaBebe <contactodanzabebe@gmail.com>
2016-07-11 01:32:49 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+ createCanvas(windowWidth, windowHeight);
+ 
+}
+
+function draw() {
+  
+  textSize(32);
+  text("DanzaBebé", 10, 30);
+  fill(0, 102, 153);
+  text("contactodanzabebe@gmail.com", 10, 60);
+  fill(0, 102, 153, 51);
+  text("mlopezcelebrarte@gmail.com", 10, 90);
+}
